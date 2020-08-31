132caa2903976d16ee427ebdf7e3424db64d81dc
tesla <akilahmed096@gmail.com>
2020-08-31 23:25:52 +0530

    fifty pdf data dded to web page

--- src/app.py
+++ src/app.py
@@ -4,34 +4,52 @@ import sys
 from flask import Flask, jsonify, request, render_template
 from nlpCode.pdfToCsv import *
 from nlpCode.frequentWords import *
-
+from nlpCode.fiftyPdf import *
 
 app = Flask(__name__)
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
+def format_data(tf, frequent):
+    new_tf = json.loads(tf.to_json())
+    new_frequent = json.loads(frequent.to_json())
+    new_frequent_dict = {}
+
+    for f in new_frequent["0"].keys():
+        new_frequent_dict[new_frequent["0"][f]] = new_frequent["1"][f]
+
+    to_be_send = {"tf_idf": new_tf["TF-IDF"], "frequent": new_frequent_dict}
+
+    return to_be_send
+
+
 @app.route("/")
 def index():
-    return render_template("upload.html")
+    data = fifty_data_func("../profiles/")
+    fify_pdf_array = []
+    for item in data:
+        (tf, frequent) = item
+
+        single_data = format_data(tf, frequent)
+
+        fify_pdf_array.append(single_data)
+
+    return render_template("upload.html", data=fify_pdf_array)
 
 
 @app.route("/upload_pdf", methods=["POST"])
 def upload_pdf():
     dataFrame = pd.DataFrame()
     target = os.path.join(APP_ROOT, "temp/")
-    print(target)
 
     if not os.path.isdir(target):
         os.mkdir(target)
 
     for file in request.files.getlist("file"):
-        print(file)
 
         filename = file.filename
         destination = "/".join([target, filename])
-
-        print(destination)
 
         file.save(destination)
         data = dataFrame.append({"data": pdfToCsv(destination, "")}, ignore_index=True)
@@ -43,19 +61,7 @@ def upload_pdf():
 
         (tf, frequent) = frequentAndEssentialWords(text)
 
-        print(tf.to_json())
-        print(frequent.to_json())
-
-        new_tf = json.loads(tf.to_json())
-        new_frequent = json.loads(frequent.to_json())
-        new_frequent_dict = {}
-
-        for f in new_frequent["0"].keys():
-            new_frequent_dict[new_frequent["0"][f]] = new_frequent["1"][f]
-
-        to_be_send = {"tf_idf": new_tf["TF-IDF"], "frequent": new_frequent_dict}
-
-        print(to_be_send)
+        to_be_send = format_data(tf, frequent)
 
         return render_template("complete.html", toBeSend=to_be_send)
 
@@ -64,13 +70,11 @@ def upload_pdf():
 def upload_text():
     dataFrame = pd.DataFrame()
     target = os.path.join(APP_ROOT, "temp")
-    print(target)
 
     if not os.path.isdir(target):
         os.mkdir(target)
 
     for file in request.files.getlist("file"):
-        print(file)
 
         filename = file.filename
         destination = "/".join([target, filename])
@@ -86,12 +90,7 @@ def upload_text():
 
         (tf, frequent) = frequentAndEssentialWords(text)
 
-        new_tf = tf.to_json(tf)
-        new_frequent = frequent.to_json(frequent)
-
-        to_be_send = {"tf": new_tf, "frequent": new_frequent}
-
-        print(to_be_send)
+        to_be_send = format_data(tf, frequent)
 
         return render_template("complete.html", toBeSend=to_be_send)
 
--- src/nlpCode/fiftyPdf.py
+++ src/nlpCode/fiftyPdf.py
@@ -1,32 +1,36 @@
-from pdfToCsv import *
-from frequentWords import *
+from nlpCode.pdfToCsv import *
+from nlpCode.frequentWords import *
 
 import json
 import pdftotext
 import pandas as pd
 
-# nltk.download("punkt")
 import os
 
-data = pd.DataFrame()
 
-for filename in os.listdir("./profiles"):
-    if filename.endswith(".pdf"):
-        data = data.append(
-            {"data": pdfToCsv(filename, "./profiles/")}, ignore_index=True
-        )
-        os.remove(filename + ".csv")
-        os.remove("./output.txt")
-        continue
-    else:
-        continue
+def fifty_data_func(dir):
 
-data.to_csv("fifty.csv")
+    data = pd.DataFrame()
 
-df = pd.read_csv("./fifty.csv")
+    fifty_data = []
 
-texts = df.data
+    for filename in os.listdir(dir):
+        if filename.endswith(".pdf"):
+            data = data.append({"data": pdfToCsv(filename, dir)}, ignore_index=True)
+            os.remove(filename + ".csv")
+            # os.remove("../output.txt")
+            continue
+        else:
+            continue
 
-for text in texts:
-    toBePrinted = frequentAndEssentialWords(text)
-    print(toBePrinted)
+    data.to_csv("fifty.csv")
+
+    df = pd.read_csv("./fifty.csv")
+
+    texts = df.data
+
+    for text in texts:
+        toBePrinted = frequentAndEssentialWords(text)
+        fifty_data.append(toBePrinted)
+
+    return fifty_data
