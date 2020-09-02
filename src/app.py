26d247c909ccadaaa2520b71ca3d0532581f9d02
tesla <akilahmed096@gmail.com>
2020-09-02 21:00:18 +0530

    heroku deploy ment

--- src/app.py
+++ src/app.py
@@ -1,99 +1,99 @@
-# import os
-# import sys
+import os
+import sys
 
-# from flask import Flask, jsonify, request, render_template
-# from src.nlpCode.pdfToCsv import *
-# from src.nlpCode.frequentWords import *
-# from src.nlpCode.fiftyPdf import *
+from flask import Flask, jsonify, request, render_template
+from src.nlpCode.pdfToCsv import *
+from src.nlpCode.frequentWords import *
+from src.nlpCode.fiftyPdf import *
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
-# APP_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-
-# def format_data(tf, frequent):
-#     new_tf = json.loads(tf.to_json())
-#     new_frequent = json.loads(frequent.to_json())
-#     new_frequent_dict = {}
-
-#     for f in new_frequent["0"].keys():
-#         new_frequent_dict[new_frequent["0"][f]] = new_frequent["1"][f]
-
-#     to_be_send = {"tf_idf": new_tf["TF-IDF"], "frequent": new_frequent_dict}
-
-#     return to_be_send
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
-# @app.route("/")
-# def index():
-#     data = fifty_data_func("./profiles/")
-#     fify_pdf_array = []
-#     for item in data:
-#         (tf, frequent) = item
+def format_data(tf, frequent):
+    new_tf = json.loads(tf.to_json())
+    new_frequent = json.loads(frequent.to_json())
+    new_frequent_dict = {}
 
-#         single_data = format_data(tf, frequent)
+    for f in new_frequent["0"].keys():
+        new_frequent_dict[new_frequent["0"][f]] = new_frequent["1"][f]
 
-#         fify_pdf_array.append(single_data)
+    to_be_send = {"tf_idf": new_tf["TF-IDF"], "frequent": new_frequent_dict}
 
-#     return render_template("upload.html", data=fify_pdf_array)
-
-
-# @app.route("/upload_pdf", methods=["POST"])
-# def upload_pdf():
-#     dataFrame = pd.DataFrame()
-#     target = os.path.join(APP_ROOT, "temp/")
-
-#     if not os.path.isdir(target):
-#         os.mkdir(target)
-
-#     for file in request.files.getlist("file"):
-
-#         filename = file.filename
-#         destination = "/".join([target, filename])
-
-#         file.save(destination)
-#         data = dataFrame.append({"data": pdfToCsv(destination, "")}, ignore_index=True)
-
-#         os.remove(destination)
-#         os.remove(destination + ".csv")
-
-#         text = data.data[0]
-
-#         (tf, frequent) = frequentAndEssentialWords(text)
-
-#         to_be_send = format_data(tf, frequent)
-
-#         return render_template("complete.html", toBeSend=to_be_send)
+    return to_be_send
 
 
-# @app.route("/upload_text", methods=["POST"])
-# def upload_text():
-#     dataFrame = pd.DataFrame()
-#     target = os.path.join(APP_ROOT, "temp")
+@app.route("/")
+def index():
+    data = fifty_data_func("./profiles/")
+    fify_pdf_array = []
+    for item in data:
+        (tf, frequent) = item
 
-#     if not os.path.isdir(target):
-#         os.mkdir(target)
+        single_data = format_data(tf, frequent)
 
-#     for file in request.files.getlist("file"):
+        fify_pdf_array.append(single_data)
 
-#         filename = file.filename
-#         destination = "/".join([target, filename])
-
-#         file.save(destination)
-
-#         data = dataFrame.append({"data": textToCsv(destination)}, ignore_index=True)
-
-#         os.remove(destination)
-#         os.remove(destination + ".csv")
-
-#         text = data.data[0]
-
-#         (tf, frequent) = frequentAndEssentialWords(text)
-
-#         to_be_send = format_data(tf, frequent)
-
-#         return render_template("complete.html", toBeSend=to_be_send)
+    return render_template("upload.html", data=fify_pdf_array)
 
 
-# # if __name__ == "__main__":
-# #     app.run(port=5000, debug=True)
+@app.route("/upload_pdf", methods=["POST"])
+def upload_pdf():
+    dataFrame = pd.DataFrame()
+    target = os.path.join(APP_ROOT, "temp/")
+
+    if not os.path.isdir(target):
+        os.mkdir(target)
+
+    for file in request.files.getlist("file"):
+
+        filename = file.filename
+        destination = "/".join([target, filename])
+
+        file.save(destination)
+        data = dataFrame.append({"data": pdfToCsv(destination, "")}, ignore_index=True)
+
+        os.remove(destination)
+        os.remove(destination + ".csv")
+
+        text = data.data[0]
+
+        (tf, frequent) = frequentAndEssentialWords(text)
+
+        to_be_send = format_data(tf, frequent)
+
+        return render_template("complete.html", toBeSend=to_be_send)
+
+
+@app.route("/upload_text", methods=["POST"])
+def upload_text():
+    dataFrame = pd.DataFrame()
+    target = os.path.join(APP_ROOT, "temp")
+
+    if not os.path.isdir(target):
+        os.mkdir(target)
+
+    for file in request.files.getlist("file"):
+
+        filename = file.filename
+        destination = "/".join([target, filename])
+
+        file.save(destination)
+
+        data = dataFrame.append({"data": textToCsv(destination)}, ignore_index=True)
+
+        os.remove(destination)
+        os.remove(destination + ".csv")
+
+        text = data.data[0]
+
+        (tf, frequent) = frequentAndEssentialWords(text)
+
+        to_be_send = format_data(tf, frequent)
+
+        return render_template("complete.html", toBeSend=to_be_send)
+
+
+# if __name__ == "__main__":
+#     app.run(port=5000, debug=True)
